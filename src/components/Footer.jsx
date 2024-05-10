3d8f862ea970d0d1d94f2bbdf9325d45b8245d0a
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-05-10 23:50:37 +0530

    w

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,31 @@
+import tflogo from "../assets/logo/Logo.png";
+import { logos } from "../data/logos";
+import "../styles/footer.css";
+
 export default function Footer() {
   return (
-    <section>
-      <div className="foot">
-        <div className="container-foot">
-          <h3>Follow Us On:</h3>
-          <ul>
-            <li>
-              <a href="#">
-                <img src="./Assets/logo/Instagram.svg" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img src="./Assets/logo/threads.svg" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img src="./Assets/logo/LinkedIn.png" />
-              </a>
-            </li>
-          </ul>
-        </div>
-        <img
-          className="logo-f"
-          src="./Assets/logo/Logo.png"
-          alt="tflogo"
-        />
-      </div>
+    <section className="footer">
+      <h3 className="footer__heading">Follow Us On:</h3>
+      <ul>
+        {logos.map(logo => (
+          <li
+            key={logo.id}
+            className="footer__lists"
+          >
+            <a href={logo.link}>
+              <img
+                src={logo.image}
+                className="footer__logos"
+              ></img>
+            </a>
+          </li>
+        ))}
+      </ul>
+      <img
+        className="footer__tflogo"
+        src={tflogo}
+        alt="techfestlogo"
+      />
     </section>
   );
 }
---
+++ src/data/logos.js
@@ -0,0 +1,20 @@
+import insta from "../assets/logo/instagram-logo.png";
+import linkedin from "../assets/logo/linkedin-square-logo-24.png";
+
+export const logos = [
+  {
+    id: 1,
+    image: insta,
+    link: "https://www.instagram.com/nm_techfest?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+  },
+  {
+    id: 2,
+    image: linkedin,
+    link: "https://www.linkedin.com/company/nm-techfest/",
+  },
+  // {
+  //   id: 3,
+  //   image: threads,
+  //   link: "https://www.threads.net/@nm_techfest?xmt=AQGzKVOTgfG6UDQeEoDPSD-v0lwdCgNzPvKHfnCW19GvG6U",
+  // },
+];
