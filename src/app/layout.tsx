e513dd5fb3eedf58e4aa3c6b1a335097f27e8d8d
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-06-06 17:59:27 +0530

    w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import Footer from "@/Components/Footer";
 import NavBar from "@/Components/NavBar";
 import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
+import { Montserrat, Open_Sans } from "next/font/google";
 import "./modern.css";
 import "./utils.scss";
 
-export const open = {
-  subsets: ["Latin"],
-  variable: "font-open",
-};
+export const open = Open_Sans({
+  subsets: ["latin"],
+});
 
 export const mons = Montserrat({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
