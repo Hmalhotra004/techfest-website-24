bd9967caddb7df8bb0bceb10a15b284b3bdddbf6
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-06-06 18:12:37 +0530

    w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,11 @@
 import Footer from "@/Components/Footer";
 import NavBar from "@/Components/NavBar";
 import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./modern.css";
 import "./utils.scss";
 
-export const montserrat = Montserrat({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-  display: "swap",
-});
-
-// export const open = Open_Sans({
-//   subsets: ["latin"],
-//   display: "swap",
-// });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "NMTechFest'24",
@@ -28,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>
+      <body className={inter.className}>
         <NavBar />
         {children}
         <Footer />
