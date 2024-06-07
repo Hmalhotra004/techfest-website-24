d16e5d524c1a2908345ca99804f1f647e8b13f62
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-06-07 13:23:02 +0530

    w

--- src/app/TechSummit/page.tsx
+++ src/app/TechSummit/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 import Cards from "@/Components/Cards";
 import Reveal from "@/Components/Reveal";
-import { panel_1 } from "@/lib/summit";
+import { artists, contact, panel_1, panel_2, speaker } from "@/lib/summit";
 import styles from "./ts.module.scss";
 
 const TechSummit = () => {
   return (
     <section className={styles.bg}>
-      <section className={`container ${styles.ts}`}>
+      <main className={`container ${styles.ts}`}>
         <Reveal x={-800}>
           <h1 className={styles.header}>TECHSUMMIT</h1>
         </Reveal>
@@ -33,9 +33,50 @@ const TechSummit = () => {
         </Reveal>
 
         <Cards data={panel_1} />
-      </section>
+
+        <Reveal x={-400}>
+          <h2 className={styles.header2}>Panel 2:</h2>
+        </Reveal>
+
+        <Reveal x={-800}>
+          <h3 className={styles.header3}>BEYOND THE SPOTLIGHT</h3>
+        </Reveal>
+
+        <Cards data={panel_2} />
+
+        <Reveal x={-800}>
+          <h2 className={styles.header2}>MUSIC ARTISTS</h2>
+        </Reveal>
+
+        <Cards data={artists} />
+
+        <Reveal x={-800}>
+          <h2 className={styles.header2}>KEYNOTE SPEAKERS</h2>
+        </Reveal>
+
+        <Cards data={speaker} />
+
+        <Contact />
+      </main>
     </section>
   );
 };
+
+const Contact = () => (
+  <footer className={styles.contact}>
+    <h2>Contact Us</h2>
+    <div className={styles.wrapper}>
+      {contact.map((data, idx) => (
+        <div
+          key={idx}
+          className={styles.inner}
+        >
+          <h3>{data.Name}</h3>
+          <p>{data.No}</p>
+        </div>
+      ))}
+    </div>
+  </footer>
+);
 
 export default TechSummit;
