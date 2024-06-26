1064fc0fa43d9caa879c06617c59cc131621f35b
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-06-26 19:16:31 +0530

    w

---
+++ src/app/SAP/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Reveal from "@/Components/Reveal";
+import styles from "./sap.module.scss";
+
+const SAP = () => {
+  return (
+    <section className={styles.bg}>
+      <main className={`container mainP`}>
+        <Reveal x={-800}>
+          <h1 className="heading">Student Ambassador Program</h1>
+        </Reveal>
+      </main>
+    </section>
+  );
+};
+
+export default SAP;
+ 
