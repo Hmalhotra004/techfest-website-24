da33f6c7b63bd56d27b06e3d7a002fac1951adab
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-06-26 17:34:30 +0530

    w

--- src/app/Contact-us/page.tsx
+++ src/app/Contact-us/page.tsx
@@ -1,10 +1,40 @@
+"use client";
+import Reveal from "@/Components/Reveal";
 import styles from "./contact.module.scss";
 
 const ContactUs = () => {
   return (
     <section className={styles.bg}>
       <main className={`container mainP`}>
-        <h1 className="heading">Contact Us</h1>
+        <Reveal x={-800}>
+          <h1 className="heading">Contact Us</h1>
+        </Reveal>
+
+        <form id={styles.form}>
+          <main id={styles.main}>
+            <div id={styles.left}>
+              <input
+                type="text"
+                id="fn"
+                placeholder="Name"
+              />
+              <input
+                type="email"
+                id="email"
+                placeholder="Email"
+              />
+              <input
+                type="tel"
+                id="phone"
+                placeholder="Phone"
+              />
+            </div>
+            <div>
+              <textarea placeholder="Text Message" />
+            </div>
+          </main>
+          <button type="submit">Send</button>
+        </form>
       </main>
     </section>
   );
