068648a5fba3a2ebf9ad8833d3e633d7e2080c6f
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-06-27 12:58:38 +0530

    w

--- src/app/SAP/page.tsx
+++ src/app/SAP/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Reveal from "@/Components/Reveal";
+import { SAPD, SAPD2, SAPH, SAPH2, SAPH3 } from "@/lib/sap";
 import styles from "./sap.module.scss";
 
 const SAP = () => {
@@ -8,6 +9,46 @@ const SAP = () => {
       <main className={`container mainP`}>
         <Reveal x={-800}>
           <h1 className="heading">Student Ambassador Program</h1>
+        </Reveal>
+
+        <Reveal x={-800}>
+          <h2 className={styles.head2}>{SAPH}</h2>
+        </Reveal>
+
+        {SAPD.map((data, idx) => {
+          return (
+            <Reveal
+              x={-400}
+              key={idx}
+            >
+              <details className={styles.details}>
+                <summary className={styles.head3}>{data.title}</summary>
+                <p className={styles.para}>{data.p}</p>
+              </details>
+            </Reveal>
+          );
+        })}
+
+        <Reveal x={-800}>
+          <h2 className={styles.head2}>{SAPH2}</h2>
+        </Reveal>
+
+        {SAPD2.map((data, idx) => {
+          return (
+            <Reveal
+              x={-400}
+              key={idx}
+            >
+              <details className={styles.details}>
+                <summary className={styles.head3}>{data.title}</summary>
+                <p className={styles.para}>{data.p}</p>
+              </details>
+            </Reveal>
+          );
+        })}
+
+        <Reveal x={-800}>
+          <h2 className={styles.head2}>{SAPH3}</h2>
         </Reveal>
       </main>
     </section>
---
+++ src/lib/sap.ts
@@ -0,0 +1,51 @@
+export const SAPH = `The NM Techfest Student Ambassador Programme is a unique opportunity for enthusiastic and driven individuals to become key representatives of the fintech and gaming community. As a Student Ambassador, you'll gain valuable experience, exposure, and a range of perks designed to enhance your skills and network.`;
+
+export const SAPD = [
+  {
+    title: "Certificates",
+    p: "Ambassadors will receive official certificates recognizing their active involvement and contribution to NM Techfest. This not only adds a valuable credential to your profile but also acknowledges your commitment to the tech community.",
+  },
+  {
+    title: "Exclusive Access to Events",
+    p: "Enjoy access to a range of NM Techfest events. Whether it's cutting-edge tech talks, workshops, or exciting competitions, Student Ambassadors get a front-row seat to the action.",
+  },
+  {
+    title: "Networking Opportunities",
+    p: "Forge connections with like-minded individuals, industry professionals, and renowned speakers. Grab the opportunity to build a network that can prove instrumental in your academic and professional journey.",
+  },
+  {
+    title: "Experience and Exposure",
+    p: "As student ambassadors, individuals will get a chance to witness an array of cutting-edge events. This exposure not only enhances their knowledge but also provides a unique networking platform.",
+  },
+  {
+    title: "Communication Skills",
+    p: "Sharpen your communication skills by representing NM Techfest in various capacities. From speaking engagements to social media interaction, this program provides a platform to enhance your ability to articulate ideas effectively",
+  },
+  {
+    title: "Exclusive Access to Summits",
+    p: "Be part of exclusive summits where you can engage with industry leaders, gain insider knowledge, and participate in discussions shaping the future of technology.",
+  },
+];
+
+export const SAPH2 = `As a Student Ambassador, you'll take on multifaceted responsibilities that contribute to the vibrant atmosphere of NM Techfest.`;
+
+export const SAPD2 = [
+  {
+    title: `Promotion on Social Media`,
+    p: `Ambassadors become the voice of NM Techfest on various social media platforms, driving engagement and excitement leading up to the event. Through compelling posts, updates, and interactive content, you'll be instrumental in creating a buzz that reaches a wide audience.`,
+  },
+  {
+    title: `Support and Guidance to Attendees`,
+    p: `As a friendly face at NM Techfest, Student Ambassadors provide support and guidance to attendees, ensuring everyone has a seamless and enriching experience. From assisting with event logistics to answering queries, Ambassadors play a crucial role in fostering a positive environment.`,
+  },
+  {
+    title: `Representative Role`,
+    p: `Ambassadors embody the spirit and values of NM Techfest, serving as enthusiastic representatives of the event. Whether interacting with participants, sponsors, or speakers, Ambassadors create a welcoming atmosphere that reflects the vibrant fintech community associated with NM Techfest.`,
+  },
+  {
+    title: `Deliverables`,
+    p: `Student Ambassadors are tasked with delivering on specific goals and initiatives. This may include generating creative content, organizing pre-event activities, or contributing ideas to enhance the overall experience. Ambassadors take pride in delivering tangible results that contribute to the success of NM Techfest.`,
+  },
+];
+
+export const SAPH3 = `Joining the NM Techfest Student Ambassador Programme is not just about being part of an event; it's about taking on meaningful responsibilities that shape the narrative of one of the most dynamic tech gatherings.`;
