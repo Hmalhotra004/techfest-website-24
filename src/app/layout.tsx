7b059f4a1ffd316788710c9a4958c7e6cd5d0470
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-06-27 16:28:11 +0530

    w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,30 @@
 import Footer from "@/Components/Footer";
 import NavBar from "@/Components/NavBar";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Chelsea_Market, Inter, Poppins } from "next/font/google";
 import "./modern.css";
 import "./utils.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--pop",
+});
+
+const chelsea = Chelsea_Market({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--chelsea",
+});
+
 export const metadata: Metadata = {
   title: "NMTechFest'24",
   description: "TechFest of NM College",
 };
+
+const font = `${poppins.variable} ${chelsea.variable}`;
 
 export default function RootLayout({
   children,
@@ -19,7 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={font}>
         <NavBar />
         {children}
         <Footer />
