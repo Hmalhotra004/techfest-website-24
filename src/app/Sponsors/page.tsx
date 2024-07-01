9c30216d62c64e1f55fc02f5266556cd07983b45
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-07-01 18:05:57 +0530

    w

--- src/app/Sponsors/page.tsx
+++ src/app/Sponsors/page.tsx
@@ -12,43 +12,43 @@ const page = () => {
         <Reveal x={-800}>
           <h1 className={styles.header1}>Our Sponsors</h1>
         </Reveal>
-
-        <div id={styles.yes}>
-          {new1.map((data, idx) => (
-            <motion.img
-              initial={{ opacity: 0, y: 100 }}
-              whileInView={{ opacity: 1, y: 0, transition: { duration: 0.7 } }}
-              viewport={{ once: true }}
-              src={data.src}
-              alt={data.name}
-              key={idx}
-            />
-          ))}
-        </div>
+        <LayImg list={new1} />
 
         <Reveal x={-800}>
           <h1 className={styles.header1}>Media Partners</h1>
         </Reveal>
-        <div id={styles.yes}>
-          {media.map((data, idx) => (
-            <motion.img
-              initial={{ opacity: 0, y: 100 }}
-              whileInView={{ opacity: 1, y: 0, transition: { duration: 0.7 } }}
-              viewport={{ once: true }}
-              src={data.src}
-              alt={data.name}
-              key={idx}
-            />
-          ))}
-        </div>
+        <LayImg list={media} />
 
         <Reveal x={-800}>
           <h1 className={styles.header1}>Past Sponsors</h1>
         </Reveal>
-
         <Carousel logos={past} />
       </main>
     </section>
+  );
+};
+
+type PropsImg = {
+  list: {
+    src: string;
+    name: string;
+  }[];
+};
+
+const LayImg = ({ list }: PropsImg) => {
+  return (
+    <div id={styles.yes}>
+      {list.map((data, idx) => (
+        <motion.img
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0, transition: { duration: 0.7 } }}
+          viewport={{ once: true }}
+          src={data.src}
+          alt={data.name}
+          key={idx}
+        />
+      ))}
+    </div>
   );
 };
 export default page;
