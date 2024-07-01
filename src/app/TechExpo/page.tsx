0cafa5e9e1fcc045aa545ab95280ef44dab4d973
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-07-01 19:01:41 +0530

    w

--- src/app/TechExpo/page.tsx
+++ src/app/TechExpo/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Reveal from "@/Components/Reveal";
-import { p1, p2 } from "@/lib/expo";
+import { p1, p2, p3, rounds, rules } from "@/lib/expo";
 import styles from "./expo.module.scss";
 
 const page = () => {
@@ -20,6 +20,48 @@ const page = () => {
         <Reveal x={-400}>
           <p className={styles.para2}>{p2}</p>
         </Reveal>
+
+        {rounds.map((data, idx) => (
+          <div key={idx}>
+            <Reveal x={-400}>
+              <h2 className={styles.header2}>{data.r}</h2>
+            </Reveal>
+
+            <Reveal x={-400}>
+              <h3 className={styles.header3}>{data.t}</h3>
+            </Reveal>
+            <Reveal x={-400}>
+              <p className={styles.para3}>{data.d}</p>
+            </Reveal>
+          </div>
+        ))}
+
+        <Reveal x={-400}>
+          <p className={styles.para2}>{p3}</p>
+        </Reveal>
+
+        {rules.map((data, idx) => (
+          <div
+            key={idx}
+            id={styles.rules}
+          >
+            <Reveal x={-800}>
+              <h2 className={styles.header2}>{data.ti}</h2>
+            </Reveal>
+            <ul id={styles.ol}>
+              {data.li.map((list, idx) => (
+                <Reveal
+                  x={-400}
+                  key={idx}
+                >
+                  <li className={styles.lists}>{list}</li>
+                </Reveal>
+              ))}
+            </ul>
+          </div>
+        ))}
+
+        <div style={{ margin: "3rem 0" }} />
       </section>
     </section>
   );
