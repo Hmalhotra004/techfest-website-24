0bd0d2b88f5ac0271bab9117e52cc68601e04eae
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-07-01 18:38:38 +0530

    w

--- src/lib/expo.ts
+++ src/lib/expo.ts
@@ -1,3 +1,34 @@
 export const p1 = `The event is designed to test participants&apos adaptability, creativity, and analytical skills, as they present their assigned unconventional careers. Following are the details and activities which will be taking place in each round respectively.`;
 
-export const p2 = `​Tech-Expo is conducting an event which comprises of three distinct stages: the pre-registration round, the pitch round, and a surprise round. The event is designed to test participants' adaptability, creativity, and analytical skills, as they present their assigned unconventional careers with a twist. Following are the details and activities which will be taking place in each round respectively.`;
+export const p2 = `Tech-Expo is conducting an event which comprises of three distinct stages: the pre-registration round, the pitch round, and a surprise round. The event is designed to test participants' adaptability, creativity, and analytical skills, as they present their assigned unconventional careers with a twist. Following are the details and activities which will be taking place in each round respectively.`;
+
+export const rounds = [
+  {
+    r: `Round 1`,
+    t: `PITCH NEXUS: Connecting ideas to opportunities`,
+    d: `Participants, upon successful registration, will receive detailed information about their assigned roles within 48 hours. These details shall revolve around an unconventional vocation (e.g., Tour guide). Participants are required to craft a detailed PowerPoint presentation (PPT) that not only introduces the unconventional career alloted to them but also includes a comprehensive analysis of the industry, emphasizing unit economy and revenue model. The submission deadline for the PPT is set before 16 February, 2024. Participants have the option to either present individually or in a pair of two. Selected participants shall proceed to the second round.`,
+  },
+  {
+    r: `Round 2`,
+    t: `BIZ MINDS`,
+    d: `This is an offline round where each team is expected to pitch the unconventional career provided to them along with their PowerPoint presentation within a time limit of 7 minutes in front of the judges, which shall also include a Q & A session. Participants are expected to provide a comprehensive detailed analysis of the designated industry, incorporating all details which shall be provided by our team after registration. The participants’ advancement to the third round shall be based on the judges’ decision, style and details of the presentation.`,
+  },
+  {
+    r: `Round 3`,
+    t: `MYSTERY ROUND`,
+    d: `The activities and details of round 3 shall be disclosed to the participants on the event day after successful selection in the second round`,
+  },
+];
+
+export const p3 = `Please note that participants are required to prepare the PowerPoint presentation outlining their ideas beforehand.`;
+
+export const rules = [
+  {
+    ti: `RULES & GUIDELINES`,
+    li: [`Participants must strictly adhere to the assigned categories for their presentations.`, `Ensure timely submission of your presentations as late submissions shall not be considered.`, `Topics will be assigned within 48 hours post-registration, and prompt submission allows more preparation time.`, `Participants have the option to present individually or in a team of two. No changes shall be allowed in the teams after registration.`, `Presentation time should not exceed 7 minutes, which shall include the Q & A session as well.`, `The decision of the judges and the organizing team shall be final.`, `Any form of cheating or misconduct will result in immediate disqualification.`],
+  },
+  {
+    ti: `GENERAL RULES & REGULATIONS`,
+    li: [`Any kind of racist comments or other slangs disrespecting any caste, gender, religion are not permitted during the game.`, `Any kind of indiscipline or misuse of mobile phones in the event can cause disqualification of the player/team.`, `The organizing committee will not be responsible for any technical failure from the participants' side.`, `Time slots shall be provided for each round. Delays in arriving at the venue can lead to disqualification.`, `The decision of the organizing team will be final.`],
+  },
+];
