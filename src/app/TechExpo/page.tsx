be6f4e5000815c58082fdd05757ae4a34c93a9f6
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-07-01 18:14:05 +0530

    w

---
+++ src/app/TechExpo/page.tsx
@@ -0,0 +1,13 @@
+import styles from "./expo.module.scss";
+
+const page = () => {
+  return (
+    <section className={styles.bg}>
+      <section className={`container mainP`}>
+        <h1>TechExpo</h1>
+      </section>
+    </section>
+  );
+};
+
+export default page;
