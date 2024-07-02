d41cf25eb5b46d7daed4e1bd1ad057df6def0c09
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-07-02 17:20:06 +0530

    w

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+import TFLogo from "@/Components/TFLogo";
+import styles from "./home.module.scss";
+
 const Home = () => {
   return (
-    <section>
-      <h1>home</h1>
-    </section>
+    <>
+      <section className={styles.bg1}>
+        <section className={`container ${styles.first}`}>
+          <div>
+            <TFLogo classname={styles.logo} />
+            <TeamBtn />
+          </div>
+        </section>
+      </section>
+    </>
   );
 };
+
+const TeamBtn = () => (
+  <button className={styles.meetbtn}>
+    <a href="/Meet-The-Team">Meet The Team</a>
+  </button>
+);
 
 export default Home;
