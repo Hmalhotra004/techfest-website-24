6c335522c358eb6d8f3b21d64682101110e1c9d1
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-07-02 17:54:39 +0530

    w

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,56 @@
+"use client";
 import TFLogo from "@/Components/TFLogo";
-import styles from "./home.module.scss";
+import { motion } from "framer-motion";
+import stylesCore from "./homeCore.module.scss";
+import stylesLogo from "./homeLogo.module.scss";
+import stylesTheme from "./homeTheme.module.scss";
 
 const Home = () => {
   return (
     <>
-      <section className={styles.bg1}>
-        <section className={`container ${styles.first}`}>
-          <div>
-            <TFLogo classname={styles.logo} />
-            <TeamBtn />
-          </div>
-        </section>
-      </section>
+      <HomeLogo />
+
+      <TeamSec />
+
+      <Theme />
     </>
   );
 };
 
+const HomeLogo = () => (
+  <section className={stylesLogo.bg}>
+    <section className={`container ${stylesLogo.first}`}>
+      <div>
+        <TFLogo classname={stylesLogo.logo} />
+        <TeamBtn />
+      </div>
+    </section>
+  </section>
+);
+
+const TeamSec = () => (
+  <section className={stylesCore.bg}>
+    <section className={`container ${stylesCore.core}`}>
+      <div>
+        <h2>Top Core Team</h2>
+      </div>
+      <div>
+        <h1>hello</h1>
+      </div>
+    </section>
+  </section>
+);
+
+const Theme = () => (
+  <section className={stylesTheme.bg}>
+    <section className={`container ${stylesTheme.theme}`}>
+      <motion.img src="/theme.png" />
+    </section>
+  </section>
+);
+
 const TeamBtn = () => (
-  <button className={styles.meetbtn}>
+  <button className={stylesLogo.meetbtn}>
     <a href="/Meet-The-Team">Meet The Team</a>
   </button>
 );
