e79c5c8885907343efb7deba4e53d5a7b5d8cbf7
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-07-03 17:29:58 +0530

    w

--- src/app/Meet-The-Team/page.tsx
+++ src/app/Meet-The-Team/page.tsx
@@ -6,69 +6,75 @@ import styles from "./mtt.module.scss";
 
 const MeetTheTeam = () => {
   return (
-    <section className={styles.bg}>
-      <section className={`container mainP`}>
-        <Reveal x={-800}>
-          <h1 className={styles.header1}>Meet The Team</h1>
-        </Reveal>
+    <>
+      <section className={styles.bg}>
+        <section className={`container mainP`}>
+          <Reveal x={-800}>
+            <h1 className={styles.header1}>Meet The Team</h1>
+          </Reveal>
 
-        <Reveal x={-400}>
-          <h2 className={styles.header2}>Top Core</h2>
-        </Reveal>
+          <Reveal x={-400}>
+            <h2 className={styles.header2}>Top Core</h2>
+          </Reveal>
 
-        <main>
-          <Card
-            src="/mtt/top/devB.png"
-            name="Dev Barot"
-            desg="Chairperson"
-          />
+          <main>
+            <Card
+              src="/mtt/top/devB.png"
+              name="Dev Barot"
+              desg="Chairperson"
+            />
 
-          <Cards data={top} />
+            <Cards data={top} />
 
-          <Card
-            src="/mtt/top/aayushpatel.png"
-            name="Aayush Patel"
-            desg="Fest Advisor"
-          />
-        </main>
+            <Card
+              src="/mtt/top/aayushpatel.png"
+              name="Aayush Patel"
+              desg="Fest Advisor"
+            />
+          </main>
+
+          <section className={styles.mtt}>
+            <Reveal x={-400}>
+              <h2 className={styles.header2}>Head of Departments</h2>
+            </Reveal>
+
+            <Reveal x={-400}>
+              <h2 className={styles.header2}>Vertical 1</h2>
+            </Reveal>
+
+            <Cards data={V1} />
+          </section>
+        </section>
       </section>
 
-      <section className={styles.mtt}>
-        <Reveal x={-400}>
-          <h2 className={styles.header2}>Head of Departments</h2>
-        </Reveal>
+      <section className={styles.bg}>
+        <section className={`container mainP`}>
+          <section className={styles.mtt}>
+            <Reveal x={-400}>
+              <h2 className={styles.header2}>Vertical 2</h2>
+            </Reveal>
 
-        <Reveal x={-400}>
-          <h2 className={styles.header2}>Vertical 1</h2>
-        </Reveal>
+            <Cards data={V2} />
+          </section>
 
-        <Cards data={V1} />
+          <section className={styles.mtt}>
+            <Reveal x={-400}>
+              <h2 className={styles.header2}>Vertical 3</h2>
+            </Reveal>
+
+            <Cards data={V3} />
+          </section>
+
+          <section className={styles.mtt}>
+            <Reveal x={-400}>
+              <h2 className={styles.header2}>Vertical 4</h2>
+            </Reveal>
+
+            <Cards data={V4} />
+          </section>
+        </section>
       </section>
-
-      <section className={styles.mtt}>
-        <Reveal x={-400}>
-          <h2 className={styles.header2}>Vertical 2</h2>
-        </Reveal>
-
-        <Cards data={V2} />
-      </section>
-
-      <section className={styles.mtt}>
-        <Reveal x={-400}>
-          <h2 className={styles.header2}>Vertical 3</h2>
-        </Reveal>
-
-        <Cards data={V3} />
-      </section>
-
-      <section className={styles.mtt}>
-        <Reveal x={-400}>
-          <h2 className={styles.header2}>Vertical 4</h2>
-        </Reveal>
-
-        <Cards data={V4} />
-      </section>
-    </section>
+    </>
   );
 };
 
